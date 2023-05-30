7c82e1cdb93af8c84c8aec47677f2a2d42061f19
Kartbllansh <movolovo93@gmail.com>
2023-05-30 09:05:06 +0300

    New

---
+++ src/main/java/com/example/lab8/Apps/AutRegController.java
@@ -0,0 +1,15 @@
+package com.example.lab8.Apps;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AutRegController {
+@FXML
+    private Button login;
+@FXML
+    private void initialize(){
+    login.setOnAction(event -> {
+
+    });
+}
+}
