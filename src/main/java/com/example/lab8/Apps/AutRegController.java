96ac2a0a5006318fdccc971f8cf87a5f21598270
Kartbllansh <movolovo93@gmail.com>
2023-05-30 12:25:54 +0300

    After update

--- src/main/java/com/example/lab8/Apps/AutRegController.java
+++ src/main/java/com/example/lab8/Apps/AutRegController.java
@@ -1,7 +1,12 @@
 package com.example.lab8.Apps;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AutRegController {
 @FXML
@@ -9,7 +14,15 @@ public class AutRegController {
 @FXML
     private void initialize(){
     login.setOnAction(event -> {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/LogIn.fxml"));
+            Scene signUpScene = new Scene(fxmlLoader.load(), 1280, 720);
 
+            Stage currentStage = (Stage) login.getScene().getWindow();
+            currentStage.setScene(signUpScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     });
 }
 }
